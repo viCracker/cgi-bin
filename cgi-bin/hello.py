b93552d7b12d3e3b6b56cdb5e1d09b65e9a41f97
Victor Mutiso <mutisovictor69@gmail.com>
2024-04-15 08:52:44 +0300

    cgi

---
+++ cgi-bin/hello.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+print(
+    """\
+Content-Type: text/html
+
+<!DOCTYPE html>
+<html lang="en">
+<body>
+<h1>Hello, World(from viCracker)</h1>
+</body>
+</html>"""
+)
+print
